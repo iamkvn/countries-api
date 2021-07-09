51cfa85c09dbfc03722561b098f82872556a9545
iamkevn <iamkevnrodriguez@gmail.com>
2021-07-08 21:09:56 -0400

    header

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+const log = console.log;
+// const slider = document.querySelector('.toggle-switch__slider');
+// const heading1 = document.querySelector('.heading-1');
+
+// const changeMode = () => {
+//   heading1.classList.toggle('dark-mode');
+//   log('hi');
+// };
+
+// slider.addEventListener('click', changeMode);
